7ad17a293ccd10bbdb04b170bf1f1cf381005a3d
Sanders Lauture <golf1052@msn.com>
2016-08-01 02:22:25 -0400

    Added list and unregister

--- AttentionPassengers.Slack/Controllers/SlackController.cs
+++ AttentionPassengers.Slack/Controllers/SlackController.cs
@@ -32,6 +32,24 @@ namespace AttentionPassengers.Slack.Controllers
             {
                 await RegisterUser(request);
             }
+            else if (request.Text.StartsWith("list registration") ||
+                request.Text.StartsWith("list signup") ||
+                request.Text.StartsWith("list sign up") ||
+                request.Text.StartsWith("list subscription"))
+            {
+                await ListRegistration(request);
+            }
+            else if (request.Text.StartsWith("unregister me") ||
+                request.Text.StartsWith("unsign me up") ||
+                request.Text.StartsWith("unsubscribe me"))
+            {
+                await UnregisterUser(request);
+            }
+            else if (request.Text == "unregister all" ||
+                    request.Text == "unsubscribe all")
+            {
+                await UnregisterAll(request);
+            }
             else if (request.Text == "times")
             {
                 await SendTimes(request);
@@ -48,6 +66,11 @@ namespace AttentionPassengers.Slack.Controllers
 
         private async Task RegisterUser(RequestBody request)
         {
+            Registration registration = await GetRegistrationInfo(request);
+            if (registration == null)
+            {
+                return;
+            }
             var filter = Builders<User>.Filter.Eq<string>("_id", request.UserId);
             User user = AppConstants.UserCollection.Find(filter).FirstOrDefault();
             if (user == null)
@@ -55,72 +78,15 @@ namespace AttentionPassengers.Slack.Controllers
                 user = new Dto.User();
                 user.Id = request.UserId;
             }
-            string linesString = "";
-            HashSet<AppConstants.DayTimes> times = new HashSet<AppConstants.DayTimes>();
-            int forIndex = request.Text.IndexOf("for");
-            if (forIndex == -1)
-            {
-                await AppConstants.SendToResponseUrl("Couln't parse register message", request.ResponseUrl);
-                return;
-            }
-            int alertsIndex = request.Text.IndexOf("alerts");
-            if (alertsIndex == -1)
-            {
-                await AppConstants.SendToResponseUrl("Couln't parse register message", request.ResponseUrl);
-                return;
-            }
-            linesString = request.Text.Substring(forIndex + "for".Length + 1, alertsIndex - forIndex - "for".Length - 2);
-            int duringIndex = request.Text.IndexOf("during");
-            if (duringIndex == -1)
-            {
-                await AppConstants.SendToResponseUrl("Couln't parse register message", request.ResponseUrl);
-                return;
-            }
-            string timesString = request.Text.Substring(duringIndex + "during".Length + 1);
-            string[] timesSplit = timesString.Split(',');
-            foreach (string time in timesSplit)
-            {
-                AppConstants.DayTimes dayTime = AppConstants.StringToDayTime(time.Trim());
-                if (dayTime == AppConstants.DayTimes.None)
-                {
-                    await AppConstants.SendToResponseUrl($"One of your times is invalid: {time.Trim()}", request.ResponseUrl);
-                    return;
-                }
-                times.Add(dayTime);
-            }
-            HashSet<string> lines = new HashSet<string>();
-            string[] linesSplit = linesString.Split(',');
-            foreach (string line in linesSplit)
-            {
-                string finalLine = line.Trim();
-                string route = (string)AppConstants.Routes[finalLine];
-                if (route == null)
-                {
-                    try
-                    {
-                        AlertHeadersByRouteObject testLine = await AppConstants.AttentionPassengers.AlertsHeadersByRoute(finalLine);
-                    }
-                    catch (Exception ex)
-                    {
-                        await AppConstants.SendToResponseUrl($"Unknown MBTA line: {finalLine}", request.ResponseUrl);
-                        return;
-                    }
-                }
-                else
-                {
-                    finalLine = route;
-                }
-                finalLine = finalLine.ToLower();
-                lines.Add(finalLine);
-            }
-            foreach (string line in lines)
+
+            foreach (string line in registration.Lines)
             {
                 string currentAlerts = string.Empty;
                 if (!user.SubscribedTimes.ContainsKey(line))
                 {
                     user.SubscribedTimes.Add(line, new HashSet<AppConstants.DayTimes>());
                 }
-                foreach (AppConstants.DayTimes time in times)
+                foreach (AppConstants.DayTimes time in registration.Times)
                 {
                     user.SubscribedTimes[line].Add(time);
                 }
@@ -143,6 +109,156 @@ namespace AttentionPassengers.Slack.Controllers
                 user,
                 new UpdateOptions { IsUpsert = true });
             await AppConstants.SendToResponseUrl("Updated subscriptions!", request.ResponseUrl);
+        }
+
+        private async Task ListRegistration(RequestBody request)
+        {
+            var filter = Builders<User>.Filter.Eq<string>("_id", request.UserId);
+            User user = AppConstants.UserCollection.Find(filter).FirstOrDefault();
+            if (user == null)
+            {
+                await AppConstants.SendToResponseUrl("Not registered for any alerts", request.ResponseUrl);
+                return;
+            }
+            string finalMessage = string.Empty;
+            foreach (var subscription in user.SubscribedTimes)
+            {
+                if (subscription.Value.Count > 0)
+                {
+                    finalMessage += subscription.Key + " line\n";
+                    foreach (var time in subscription.Value)
+                    {
+                        finalMessage += $"\t{AppConstants.DayTimeToString(time)}\n";
+                    }
+                }
+            }
+            if (finalMessage == string.Empty)
+            {
+                finalMessage = "Not registered for any alerts";
+            }
+            await AppConstants.SendToResponseUrl(finalMessage, request.ResponseUrl);
+        }
+
+        private async Task UnregisterUser(RequestBody request)
+        {
+            Registration registration = await GetRegistrationInfo(request);
+            if (registration == null)
+            {
+                return;
+            }
+            var filter = Builders<User>.Filter.Eq<string>("_id", request.UserId);
+            User user = AppConstants.UserCollection.Find(filter).FirstOrDefault();
+            if (user == null)
+            {
+                await AppConstants.SendToResponseUrl("Not registered for any alerts", request.ResponseUrl);
+                return;
+            }
+
+            foreach (string line in registration.Lines)
+            {
+                if (user.SubscribedTimes.ContainsKey(line))
+                {
+                    foreach (AppConstants.DayTimes time in registration.Times)
+                    {
+                        if (user.SubscribedTimes[line].Contains(time))
+                        {
+                            user.SubscribedTimes[line].Remove(time);
+                        }
+                    }
+                    if (user.SubscribedTimes[line].Count == 0)
+                    {
+                        user.SubscribedTimes.Remove(line);
+                    }
+                }
+            }
+            AppConstants.UserCollection.ReplaceOne(Builders<User>.Filter.Eq<string>("_id", user.Id),
+                user,
+                new UpdateOptions { IsUpsert = true });
+            await AppConstants.SendToResponseUrl("Updated subscriptions!", request.ResponseUrl);
+        }
+
+        private async Task UnregisterAll(RequestBody request)
+        {
+            var filter = Builders<User>.Filter.Eq<string>("_id", request.UserId);
+            User user = AppConstants.UserCollection.Find(filter).FirstOrDefault();
+            if (user == null)
+            {
+                await AppConstants.SendToResponseUrl("Not registered for any alerts", request.ResponseUrl);
+                return;
+            }
+            List<string> keys = user.SubscribedTimes.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
+            {
+                user.SubscribedTimes.Remove(keys[i]);
+            }
+            AppConstants.UserCollection.ReplaceOne(Builders<User>.Filter.Eq<string>("_id", user.Id),
+                user,
+                new UpdateOptions { IsUpsert = true });
+            await AppConstants.SendToResponseUrl("Updated subscriptions!", request.ResponseUrl);
+        }
+
+        private async Task<Registration> GetRegistrationInfo(RequestBody request)
+        {
+            Registration registration = new Registration();
+            string linesString = "";
+            int forIndex = request.Text.IndexOf("for");
+            if (forIndex == -1)
+            {
+                await AppConstants.SendToResponseUrl("Couln't parse message", request.ResponseUrl);
+                return null;
+            }
+            int alertsIndex = request.Text.IndexOf("alerts");
+            if (alertsIndex == -1)
+            {
+                await AppConstants.SendToResponseUrl("Couln't parse message", request.ResponseUrl);
+                return null;
+            }
+            linesString = request.Text.Substring(forIndex + "for".Length + 1, alertsIndex - forIndex - "for".Length - 2);
+            int duringIndex = request.Text.IndexOf("during");
+            if (duringIndex == -1)
+            {
+                await AppConstants.SendToResponseUrl("Couln't parse message", request.ResponseUrl);
+                return null;
+            }
+            registration.Times = new HashSet<AppConstants.DayTimes>();
+            string timesString = request.Text.Substring(duringIndex + "during".Length + 1);
+            string[] timesSplit = timesString.Split(',');
+            foreach (string time in timesSplit)
+            {
+                AppConstants.DayTimes dayTime = AppConstants.StringToDayTime(time.Trim());
+                if (dayTime == AppConstants.DayTimes.None)
+                {
+                    await AppConstants.SendToResponseUrl($"One of your times is invalid: {time.Trim()}", request.ResponseUrl);
+                    return null;
+                }
+                registration.Times.Add(dayTime);
+            }
+            registration.Lines = new HashSet<string>();
+            string[] linesSplit = linesString.Split(',');
+            foreach (string line in linesSplit)
+            {
+                string finalLine = line.Trim();
+                string route = (string)AppConstants.Routes[finalLine];
+                if (route == null)
+                {
+                    try
+                    {
+                        AlertHeadersByRouteObject testLine = await AppConstants.AttentionPassengers.AlertsHeadersByRoute(finalLine);
+                    }
+                    catch (Exception ex)
+                    {
+                        await AppConstants.SendToResponseUrl($"Unknown MBTA line: {finalLine}", request.ResponseUrl);
+                        return null;
+                    }
+                }
+                else
+                {
+                    finalLine = route;
+                }
+                finalLine = finalLine.ToLower();
+                registration.Lines.Add(finalLine);
+            }
+            return registration;
         }
 
         private async Task GetCurrentAlerts(RequestBody request)
@@ -192,9 +308,12 @@ namespace AttentionPassengers.Slack.Controllers
         {
             string helpText = "/mbta help - this help text\n";
             helpText += "/mbta times - valid times for register command\n";
-            helpText += "/mbta register me for _lines (comman delimited)_ alerts during _times (comman delimited)_ - receive live alerts for specified lines during specified times\n";
+            helpText += "/mbta register me for _lines (comma delimited)_ alerts during _times (comma delimited)_ - receive live alerts for specified lines during specified times\n";
             helpText += "\tExamples: /mbta register me for red, orange, e line alerts during am peak, midday, pm peak, evening\n";
             helpText += "\t/mbta register me for red line alerts during midday\n";
+            helpText += "/mbta list registration - list active registrations\n";
+            helpText += "/mbta unregister me for _lines (comma delimited)_ alerts during _times (comma delimited)_ - unregister from alerts\n";
+            helpText += "/mbta unregister all - unregister from all alert registrations\n";
             helpText += "/mbta _line_ - get current alerts for the given line\n";
             helpText += "\tExamples: /mbta red line - current red line alerts\n";
             helpText += "\t/mbta 1 - current alerts for the 1 bus\n";
---
+++ AttentionPassengers.Slack/Registration.cs
@@ -0,0 +1,14 @@
+﻿using AttentionPassengers.Slack.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AttentionPassengers.Slack
+{
+    public class Registration
+    {
+        public HashSet<string> Lines { get; set; }
+        public HashSet<AppConstants.DayTimes> Times { get; set; }
+    }
+}
